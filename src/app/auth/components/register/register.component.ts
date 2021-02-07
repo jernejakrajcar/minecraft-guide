7599d872b418c216df12e5b32adaaf78c5ea8d3f
jernejakrajcar <jerneja.krajcar@scv.si>
2021-02-07 13:41:00 +0100

    added validation for login and register

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -12,4 +13,9 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onSubmit(f: NgForm) {
+    console.log(f.value); //{ first: '', last: ''}
+    console.log(f.valid); //false
+  }
+  
 }
